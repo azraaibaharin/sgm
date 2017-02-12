8ae00978fe6894d714dc8a1be1a926365fa3cfe3
azraaibaharin <dominoseffect@gmail.com>
2017-02-12 23:42:39 +0100

    Update email template for order submitted

--- resources/views/emails/ordersubmitted.blade.php
+++ resources/views/emails/ordersubmitted.blade.php
@@ -1,12 +1,12 @@
 <h1>New order received!</h1>
 <p>
-	<label>Order id:</label> {{ $order->id }}
-	<label>Reference no:</label> {{ $order->reference_number }}
-	<label>Status:</label> {{ $order->reference_number }}
-	<label>Name:</label> {{ $order->name }}
-	<label>Email:</label> {{ $order->email }}
-	<label>Phone number:</label> {{ $order->phone_number }}
-	<label>Address:</label> {{ $order->address }}
+	<label>Order id:</label> {{ $order->id }}<br>
+	<label>Reference no:</label> {{ $order->reference_number }}<br>
+	<label>Status:</label> {{ $order->reference_number }}<br>
+	<label>Name:</label> {{ $order->name }}<br>
+	<label>Email:</label> {{ $order->email }}<br>
+	<label>Phone number:</label> {{ $order->phone_number }}<br>
+	<label>Address:</label> {{ $order->address }}<br>
 </p>
 <table class="table table-hover table-bordered">
 	<thead>
@@ -19,34 +19,32 @@
 	<tbody>
 		@foreach($content as $row) 
 			<tr>
-				<td>
-					<a href="{{ url('products/'.$row->id) }}">{{ $row->name }} - <small>{{ $row->options['color'] }}</small></a>
-				</td>
-				<td class="text-center">
-					<a href="{{ url('cart/'.$row->rowId.'/remove') }}">- </a>| {{ $row->qty }} | <a href="{{ url('cart/'.$row->rowId.'/add') }}">+</a></td>
+				<td>{{ $row->name }} - {{ $row->options['color'] }}</td>
+				<td class="text-center">{{ $row->qty }}</td>
 				<td class="text-center">RM{{ $row->total }}</td>
 			</tr>
 		@endforeach
 	</tbody>
 	<tfoot>
         <tr class="active">
+	        <td> </td>
             <td class="text-center" style="vertical-align:middle;">Total</td>
             <td class="text-center" style="vertical-align:middle;">RM{{ $order->total_price }}</td>
         </tr>
         @if($order->coupon_total_value > 0)
 			<tr class="active">
-				<td>&nbsp</td>
+				<td> </td>
 				<td class="text-center">Discount</td>
 				<td class="text-center">-RM{{ $order->coupon_total_value }}</td>
 			</tr>
 		@endif
 		<tr class="active">
-			<td>&nbsp</td>
+			<td> </td>
 			<td class="text-center">Delivery</td>
 			<td class="text-center">RM{{ $order->delivery_cost }}</td>
 		</tr>
 		<tr class="active">
-            <td>&nbsp</td>
+            <td> </td>
             <td class="text-center" style="vertical-align:middle;">Final Price</td>
             <td class="text-center" style="vertical-align:middle;">RM{{ $order->final_price }}</td>
         </tr>
