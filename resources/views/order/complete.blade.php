6b17ca19882ab81164905f12e6911a18867a3bd4
azraaibaharin <dominoseffect@gmail.com>
2017-02-12 23:48:59 +0100

    Update email template for order submitted

--- resources/views/order/complete.blade.php
+++ resources/views/order/complete.blade.php
@@ -8,9 +8,9 @@
 <div class="container">
 	<div class="row">
 		<div class="col-md-12">
-			<h2>{{ $message }}</h2>
-			<h3>Your order reference number is: {{ $referenceNumber }}</h3>
-			<a class="btn btn-link" href="{{ url('products') }}">Return to Products</a>
+			<h4>{{ $message }}</h4>
+			<h5>Your order reference number is: {{ $referenceNumber }}</h5>
+			<a href="{{ url('products') }}">Return to Products</a>
 		</div>
 	</div>
 </div>
